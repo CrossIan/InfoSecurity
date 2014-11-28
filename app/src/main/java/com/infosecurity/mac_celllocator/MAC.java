b5bc6e7483416766e9d1ddae708f1b02e183ef8e
IanCross <cross.395@buckeyemail.osu.edu>
2014-11-28 13:53:38 -0500

    kevins files

--- app/src/main/java/com/infosecurity/mac_celllocator/MAC.java
+++ app/src/main/java/com/infosecurity/mac_celllocator/MAC.java
@@ -1,21 +1,88 @@
 package com.infosecurity.mac_celllocator;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.Context;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import java.nio.channels.Channel;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.channels.Channel;
 
 
 public class MAC extends Activity {
 
     final Context context = this;
 
+    IntentFilter mIntentFilter;
+
+    private String ipstring;
+
+    public String[] compIPs;
+
+    public double userLatitude, userLongitude, otherLatitude, otherLongitude;
+
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+    BroadcastReceiver mReceiver;
+
+    WifiP2pManager.PeerListListener myPeerListListener;
+
+    public String GetUserIPAddress() {
+        WifiManager mWifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+        WifiInfo mWifiInfo = mWifiManager.getConnectionInfo();
+
+        return (( mWifiInfo.getIpAddress() & 0xFF)+ "."+((mWifiInfo.getIpAddress() >> 8 ) & 0xFF)+
+                "."+((mWifiInfo.getIpAddress() >> 16 ) & 0xFF)+"."+((mWifiInfo.getIpAddress() >> 24 ) & 0xFF));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mac);
+        compIPs = new String[3];
+
+        compIPs[0]="User1-IPAddress";
+        compIPs[1]="User2-IPAddress";
+        compIPs[2]="User3-IPAddress";
+
+        ipstring = GetUserIPAddress();
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        otherLatitude = 39.0995;
+        otherLongitude = -84.5172;
     }
 
 
@@ -38,6 +105,8 @@ public class MAC extends Activity {
                 break;
             case R.id.activity_map:
                 Intent map = new Intent(context, map.class);
+                map.putExtra("otherlatitudeMAC",otherLatitude);
+                map.putExtra("otherlongitudeMAC",otherLongitude);
                 startActivity(map);
                 break;
             case R.id.activity_triangle:
@@ -55,5 +124,65 @@ public class MAC extends Activity {
         //Return false to allow normal menu processing to proceed,
         //true to consume it here.
         return false;
+    }
+    public void sendSelfIP(View view){
+        TextView text=(TextView)findViewById(R.id.useriptext);
+        text.setText(ipstring);
+    }
+
+    public void sendCompIPs(View view){
+        TextView text=(TextView)findViewById(R.id.otheriptext1);
+        TextView text2=(TextView)findViewById(R.id.otheriptext2);
+        TextView text3=(TextView)findViewById(R.id.otheriptext3);
+        text.setText(compIPs[0]);
+        text2.setText(compIPs[1]);
+        text3.setText(compIPs[2]);
+    }
+
+    /* register the broadcast receiver with the intent values to be matched */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+    public void connectToPeers(View view)
+    {
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(context, "Searching for Devices", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                Toast.makeText(context, "Connection-Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void connectToIndividual(WifiP2pDevice device)
+    {
+        //obtain a peer from the WifiP2pDeviceList
+        WifiP2pConfig config = new WifiP2pConfig();
+        config.deviceAddress = device.deviceAddress;
+        mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                //success logic
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                //failure logic
+            }
+        });
     }
 }
--- app/src/main/java/com/infosecurity/mac_celllocator/map.java
+++ app/src/main/java/com/infosecurity/mac_celllocator/map.java
@@ -3,22 +3,112 @@ package com.infosecurity.mac_celllocator;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import javax.crypto.Mac;
 
 
-public class map extends Activity {
+public class map extends FragmentActivity {
 
     final Context context = this;
+    public double userLatitude;
+    public double userLongitude;
+    public double otherLatitude;
+    public double otherLongitude;
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
+    private LatLng userLoc, otherLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        otherLatitude = 39.1005;
+        otherLongitude = -84.5162;
+        otherLoc = new LatLng(otherLatitude,otherLongitude);
+
+        userLatitude = 39.1000;
+        userLongitude = -84.5167;
+
+        Intent iin= getIntent();
+        Bundle b = iin.getExtras();
+
+
+
+        if(b!=null)
+        {
+            double j = b.getDouble("otherlatitudeMAC");
+            otherLatitude = j;
+            double i = b.getDouble("otherlongitudeMAC");
+            otherLongitude = i;
+        }
+
+        setUpMapIfNeeded();
+        userLoc = new LatLng(userLatitude, userLongitude);
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        // mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
 
@@ -59,4 +149,24 @@ public class map extends Activity {
         //true to consume it here.
         return false;
     }
+
+    public void placeUserPin(View view)
+    {
+        TextView text=(TextView)findViewById(R.id.userlat);
+        TextView text2=(TextView)findViewById(R.id.userlong);
+        text.setText(""+userLatitude);
+        text2.setText(""+userLongitude);
+        mMap.addMarker(new MarkerOptions().position(new LatLng(userLatitude, userLongitude)).title("User Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLoc,18.0f));
+    }
+
+    public void placeOtherPin(View view)
+    {
+        TextView text=(TextView)findViewById(R.id.otherlat);
+        TextView text2=(TextView)findViewById(R.id.otherlong);
+        text.setText(""+otherLatitude);
+        text2.setText(""+otherLongitude);
+        mMap.addMarker(new MarkerOptions().position(new LatLng(otherLatitude, otherLongitude)).title("Other Location"));
+    }
 }
+
